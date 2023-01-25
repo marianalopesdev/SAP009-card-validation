b997e6b68d212cc49e03f4954cd07fc84e1cf315
marianalopesdev <marianalopes.dev@gmail.com>
2023-01-25 01:27:29 -0300

    maskify feature implemented

--- src/validator.js
+++ src/validator.js
@@ -3,7 +3,9 @@ const validator = {
 };
 
 const input = document.getElementById("cardNumber");
-const isValidLabel = document.getElementById("labelText");
+const isValidLabel = document.getElementById("isValid");
+const maskedCardNumberLabel = document.getElementById("maskedCardNumber");
+const cardNumberInput = document.getElementById("cardNumber");
 
 input.addEventListener("keypress", handleKeyPressJustNumbers);
 
@@ -19,7 +21,7 @@ export function handleKeyPressJustNumbers(e) {
 export function validateCard(e) {
   e.preventDefault();
 
-  const cardNumber = document.getElementById("cardNumber").value;
+  const cardNumber = cardNumberInput.value;
 
   if (cardNumber !== "") {
     
@@ -37,6 +39,11 @@ export function checkCardNUmber(reverseCardNumber) {
   
   for (let i = 0; i < reverseCardNumber.length; i++) {
     if ((i+1) % 2 === 0) {
+
+      //refatorar com IF perguntando se o número multiplicado é maior que 10
+      // se sim -9 e somatotal
+      //se nao soma total
+
       const evenPositionNumberMultiplied = reverseCardNumber[i] * 2;
       const evenPositionNumberMultipliedArray = [];
       evenPositionNumberMultipliedArray.push(evenPositionNumberMultiplied);
@@ -53,10 +60,17 @@ export function checkCardNUmber(reverseCardNumber) {
     }
   }
   
+  const cardNumber = cardNumberInput.value;
+  const maskedValue = "#".repeat(12) + cardNumber.substr(12);
+  
+  maskedCardNumberLabel.innerHTML = maskedValue;
+
   if (totalSum % 10 === 0) {
-    alert("cartão válido");
+    
+    isValidLabel.innerHTML = "Cartão Válido";
   } else {
-    alert("cartão inválido");
+    
+    isValidLabel.innerHTML = "Cartão Inválido";
   }
   
 }
