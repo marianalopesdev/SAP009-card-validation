169dbb9e86d147648a82f75f6021001cd6022c6d
marianalopesdev <marianalopes.dev@gmail.com>
2023-02-01 02:39:57 -0300

    ReadMe update, css modifications

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import validator from "./validator.js";
 
-
 const input = document.getElementById("cardNumber");
+const isValidLabel = document.getElementById("isValidLabel");
+const maskedCardNumberLabel = document.getElementById("maskedCardNumber");
+const cardNumberInput = document.getElementById("cardNumber");
+const button = document.getElementById("sendButton");
+
 input.addEventListener("keypress", handleKeyPressJustNumbers);
+button.addEventListener("click", validateCard);
 
 function handleKeyPressJustNumbers(e) {
   const key = e.charCode || e.keyCode;
@@ -12,11 +17,37 @@ function handleKeyPressJustNumbers(e) {
   }
 }
 
+export function validateCard(event) {
+  event.preventDefault();
+  
+  const cardNumber = cardNumberInput.value;
+
+  if (cardNumber.length === 16) {
+    const cardNumberArray = cardNumber.split("").map(Number);
+    const reverseCardNumber = cardNumberArray.reverse();
+
+    const validateCardResult = validator.isValid(reverseCardNumber);
+    const maskedNumber = validator.maskify(cardNumber);
+
+    maskedCardNumberLabel.innerHTML = maskedNumber;    
+    isValidLabel.classList.remove("valid","invalid");
+    
+    if (validateCardResult === true) {
+      isValidLabel.classList.add("valid");
+      isValidLabel.innerHTML = "VALID CARD";
+    } else {
+      isValidLabel.classList.add("invalid");
+      isValidLabel.innerHTML = "INVALID CARD";
+    } 
+    cardNumberInput.value = "";   
+  }   
+  else if (cardNumber !== "") {
+    alert("Type a valid card number (16 digits)");
+  }
+  else {
+    alert("Type the card number");
+  }
+}
 
 
 console.log(validator);
-
-import { validateCard,  } from "./validator.js";
-window.validateCard = validateCard;
-//window.checkCardNUmber = checkCardNUmber;
-
--- src/validator.js
+++ src/validator.js
@@ -1,35 +1,8 @@
-const isValidLabel = document.getElementById("isValidLabel");
-const maskedCardNumberLabel = document.getElementById("maskedCardNumber");
-const cardNumberInput = document.getElementById("cardNumber");
-
-export function validateCard(e) {
-  e.preventDefault();
-
-  const cardNumber = cardNumberInput.value;
-
-  if (cardNumber !== "") {
-    const cardNumberArray = cardNumber.split("").map(Number);
-    const reverseCardNumber = cardNumberArray.reverse();
-    const validateCardResult = validator.isValid(reverseCardNumber);
-    const maskedNumber = validator.maskify(cardNumber);
-
-    maskedCardNumberLabel.innerHTML = maskedNumber;
-
-    if (validateCardResult === true) {
-      isValidLabel.innerHTML = "CARTAO VÁLIDO";
-    } else {
-      isValidLabel.innerHTML = "CARTAO INVÁLIDO";
-    }    
-  }   
-  else {
-    alert("Type the card number");
-  }
-}
-
 const validator = {
+ 
   isValid: function isValid(reverseCardNumber) {
     let totalSum = 0;
-    //alert("entrou");
+    
     for (let i = 0; i < reverseCardNumber.length; i++) {
       if ((i + 1) % 2 === 0) {
         
@@ -62,4 +35,5 @@ const validator = {
     }
   },
 };
+
 export default validator;
